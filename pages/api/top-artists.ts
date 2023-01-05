4a2b764ba11946367c0efce614eb247eb6c1e6a9
Anil Seervi <61609033+AnilSeervi@users.noreply.github.com>
2023-01-05 11:45:18 +0530

    feat: :wrench: use swc to minify js bundles (#13)
    * feat: :wrench: use swc to minify js bundles
    
    * chore: :arrow_up: bump packages to latest
    
    * chore: :arrow_up: update more packages
    
    * fix: update prisma relationMode
    
    * chore: udpate runtime config from experimental to edge

--- pages/api/top-artists.ts
+++ pages/api/top-artists.ts
@@ -2,7 +2,7 @@ import { getTopArtists } from 'lib/spotify';
 import { type NextRequest } from 'next/server';
 
 export const config = {
-  runtime: 'experimental-edge'
+  runtime: 'edge'
 };
 
 export default async function handler(req: NextRequest) {
