5577d4e5f8fc11e6fa7358be24d30ad6aa0788f4
Anil Seervi <61609033+AnilSeervi@users.noreply.github.com>
2023-03-12 13:41:30 +0530

    chore: :art: replace next Image with normal image

--- components/TopArtistsIcons.tsx
+++ components/TopArtistsIcons.tsx
@@ -1,19 +1,18 @@
 import { Artist } from 'lib/types';
-import Image from 'next/image';
 
 const formatNumberToIntl = (number) =>
   new Intl.NumberFormat('en-US').format(number);
 
 export default function TopArtistsIcons({ data }: { data: Artist[] }) {
   if (!data) return null;
-
   return (
     <div className="flex flex-wrap items-center gap-4">
       {data.map((artist, index) => (
         <div className="flex flex-col items-center" key={index}>
           <div className="flex min-w-[42px]">
             <a href={artist.artistUrl} target="_blank" rel="noreferrer">
-              <Image
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
                 src={artist.image}
                 alt={artist.name}
                 height={42}
