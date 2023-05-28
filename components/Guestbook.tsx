2e89226a2889a6bfd5f862c7cd3096720a8ce836
Anil Seervi <61609033+AnilSeervi@users.noreply.github.com>
2023-05-28 15:10:50 +0530

    chore: :art: separate client component

--- components/Guestbook.tsx
+++ components/Guestbook.tsx
@@ -1,55 +1,8 @@
 import { Suspense } from 'react';
-
 import { GuestbookType } from 'lib/types';
 import { SignUsingDiscord, SignUsingGitHub } from 'app/guestbook/actions';
 import FormView from 'app/guestbook/form';
-
-function GuestbookEntry({ entry, user }) {
-  const deleteEntry = async (e) => {
-    e.preventDefault();
-
-    await fetch(`/api/guestbook/${entry.id}`, {
-      method: 'DELETE'
-    });
-
-    window.location.reload();
-  };
-
-  return (
-    <div className="flex flex-col space-y-2">
-      <div className="w-full break-words text-gray-900 dark:text-gray-50">
-        {entry.body}
-      </div>
-      <div className="flex items-center space-x-3">
-        <p className="text-sm text-gray-500">{entry.created_by}</p>
-        <span className=" text-gray-200 dark:text-gray-800">/</span>
-        <p className="text-sm text-gray-400 dark:text-gray-600">
-          <time dateTime={entry.updated_at}>
-            {new Intl.DateTimeFormat('en-US', {
-              year: 'numeric',
-              month: 'short',
-              day: 'numeric',
-              hour: 'numeric',
-              minute: 'numeric',
-              dayPeriod: 'short'
-            }).format(new Date(entry.updated_at))}
-          </time>
-        </p>
-        {user && entry.created_by === user.name && (
-          <>
-            <span className="text-gray-200 dark:text-gray-800">/</span>
-            <button
-              className="text-sm text-red-600 dark:text-red-400"
-              onClick={deleteEntry}
-            >
-              Delete
-            </button>
-          </>
-        )}
-      </div>
-    </div>
-  );
-}
+import Entry from 'app/guestbook/entry';
 
 export default function Guestbook({ entries, session }) {
   return (
@@ -72,7 +25,7 @@ export default function Guestbook({ entries, session }) {
       <div className="mt-4 space-y-8">
         <Suspense fallback={null}>
           {entries?.map((entry: GuestbookType) => (
-            <GuestbookEntry key={entry.id} entry={entry} user={session?.user} />
+            <Entry key={entry.id} entry={entry} user={session?.user} />
           ))}
         </Suspense>
       </div>
